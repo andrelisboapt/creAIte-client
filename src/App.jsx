025383adc20c6551a6e9d4d29352b078ceb8026f
André Lisboa <andrelisboapt@hotmail.com>
2023-03-10 10:30:06 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import EditProfile from './pages/EditProfile'
+import Generator from './pages/Generator'
+import CreatePost from './pages/CreatePost'
+import Feed from './pages/Feed'
+import CommentPage from './pages/CommentPage'
+import Navbar from './components/Navbar'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar /> 
+
+
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/profile/edit" element={<EditProfile />}/>
+      <Route path="/generator" element={<Generator />}/>
+      <Route path="/post/create" element={<CreatePost />}/>
+      <Route path="/feed" element={<Feed />}/>
+      <Route path="/feed/post/comment" element={<CommentPage />}/>
+    </Routes>
+      
+      
     </div>
   )
 }
