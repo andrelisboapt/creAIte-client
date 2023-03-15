c70e15f4735849ed0879b2757ddedf57d1543191
André Lisboa <andrelisboapt@hotmail.com>
2023-03-15 18:05:08 +0000

    Co-authored-by: Lucas Zaquine <zaquinelucas@gmail.com> Co-authored-by: Tomás Belmar  <TomBelmar@users.noreply.github.com> Co-authored-by: André Ferreira <MrAFerreira@users.noreply.github.com>

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -7,7 +7,7 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import PlaceIcon from "@mui/icons-material/Place";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Posts from "../../components/posts/Posts"
+import Posts from "../../components/posts/Posts";
 import axios from "axios";
 import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../context/authContext";
@@ -15,24 +15,16 @@ import { useParams } from "react-router-dom";
 import Gallery from "../../components/gallery/Gallery";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
-import LogoutIcon from '@mui/icons-material/Logout';
-import EditIcon from '@mui/icons-material/Edit';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-
-
+import LogoutIcon from "@mui/icons-material/Logout";
+import EditIcon from "@mui/icons-material/Edit";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const Profile = () => {
-  
-  const {logout} = useContext(AuthContext)
+  const { logout } = useContext(AuthContext);
 
- 
   const [user, setUser] = useState([]);
 
-  
-
   const storedToken = localStorage.getItem("authToken");
-
-  
 
   const getUser = async () => {
     try {
@@ -42,78 +34,62 @@ const Profile = () => {
           headers: { Authorization: `Bearer ${storedToken}` },
         }
       );
-      console.log(response)
+      console.log(response);
       setUser(response.data);
-      
     } catch (error) {
       console.log(error);
     }
   };
- 
-
 
   useEffect(() => {
     getUser();
-
   }, []);
-
-
-
-  
 
   return (
     <div className="profile">
-   
       <div className="images">
         <img
           src="https://wallpaperaccess.com/full/8351209.gif"
           alt=""
           className="cover"
         />
-        <img
-          src={user.profileImage}
-          alt=""
-          className="profilePic"
-        />
+        <img src={user.profileImage} alt="" className="profilePic" />
       </div>
       <div className="profileContainer">
         <div className="uInfo">
           <div className="left editIcon">
-          <Link to="/profile/edit"><EditIcon>Edit</EditIcon>  </Link>
-          
+            <Link to="/profile/edit">
+              <EditIcon>Edit</EditIcon>{" "}
+            </Link>
           </div>
           <div className="center">
-            <span>{user.firstName} {user.lastName}</span>
+            <span>
+              {user.firstName} {user.lastName}
+            </span>
             <div className="info">
               <div className="item">
-              <a className="socialmedia" href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="medium" />
-            </a>
-            <a className="socialmedia" href="http://facebook.com">
-              <InstagramIcon fontSize="medium" />
-            </a>
-            <a className="socialmedia" href="http://facebook.com">
-              <TwitterIcon fontSize="medium" />
-            </a>
-            <a className="socialmedia" href="http://facebook.com">
-              <LinkedInIcon fontSize="medium" />
-            </a>
+                <a className="socialmedia" href="http://facebook.com">
+                  <FacebookTwoToneIcon fontSize="medium" />
+                </a>
+                <a className="socialmedia" href="http://facebook.com">
+                  <InstagramIcon fontSize="medium" />
+                </a>
+                <a className="socialmedia" href="http://facebook.com">
+                  <TwitterIcon fontSize="medium" />
+                </a>
+                <a className="socialmedia" href="http://facebook.com">
+                  <LinkedInIcon fontSize="medium" />
+                </a>
               </div>
-              
             </div>
-              {user.aboutMe}
-            
+            {user.aboutMe}
           </div>
           <div className="right Exitbtn">
-          
-          <ExitToAppIcon onClick={logout}/>
-      
+            <ExitToAppIcon onClick={logout} />
           </div>
         </div>
-        <Gallery user={user}/>
-     
+        <Gallery user={user} />
       </div>
-      
     </div>
   );
 };
